c81c7afaf769b1d571e49cf2c1f1f57f846e3e6e
dark <Abrekovalim38702@gmail.com>
2023-07-14 21:59:23 +0300

    Last Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+from app.settings import config, BASE_DIR
+from app.store.database.accessor import PostgresAccessor
+
+
+def setup_config(application):
+    application["config"] = config
+
+
+def setup_external_libraries(application):
+    aiohttp_jinja2.setup(
+        application,
+        loader=jinja2.FileSystemLoader(f"{BASE_DIR}/templates"),
+    )
+
+
+# настроим url-пути для доступа к нашему будущему приложению
+def setup_routes(application):
+    from app.forum.routes import setup_routes as setup_forum_routes
+    setup_forum_routes(application)
+
+
+def setup_accessors(application):
+    application['db'] = PostgresAccessor()
+    application['db'].setup(application)
+
+
+def setup_app(application):
+    setup_config(application)
+    setup_accessors(application)
+    setup_external_libraries(application)
+    setup_routes(application)
+
+
+app = web.Application()  # инициализируем наш веб-сервер
+
+if __name__ == "__main__":
+    setup_app(app)
+    web.run_app(
+        app,
+        host=config["common"]["host"],
+        port=config["common"]["port"]
+    )
